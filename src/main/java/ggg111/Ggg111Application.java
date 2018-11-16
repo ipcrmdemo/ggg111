c24f5270632429db8c7b812722bef89135b32c7b
Matt Cadorette <mcadorette@gmail.com>
2018-11-16 10:33:34 -0500

    Initial commit from Atomist

---
+++ src/main/java/ggg111/Ggg111Application.java
@@ -0,0 +1,12 @@
+package ggg111;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ggg111Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ggg111Application.class, args);
+	}
+}
